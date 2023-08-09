4a6caaafa179a6508befd276b405383762538f3d
hnur.simsek <hnur.simsek@HAVANUR>
2023-08-09 22:50:33 +0300

    Yeni contoroller bilgisi eklendi.

---
+++ EgitimProjeAsp/Controllers/KitapTuruController.cs
@@ -0,0 +1,22 @@
+﻿using EgitimProjeAsp.Models;
+using EgitimProjeAsp.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimProjeAsp.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+
+        private readonly UygulamaDBContext _uygulamaDBContext;
+
+        public KitapTuruController(UygulamaDBContext context)
+        {
+            _uygulamaDBContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<KitapTuru> objKitapTuruList = _uygulamaDBContext.KitapTuruleri.ToList();       
+            return View();
+        }
+    }
+}
