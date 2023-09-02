f90dce9668fddef379af2f65ec69c6d6729a65b4
hnur.simsek <hnur.simsek@HAVANUR>
2023-09-02 20:12:48 +0300

    Güncelle ve sil actionları ve uyarı mesajları

--- EgitimProjeAsp/Controllers/KitapTuruController.cs
+++ EgitimProjeAsp/Controllers/KitapTuruController.cs
@@ -15,7 +15,7 @@ namespace EgitimProjeAsp.Controllers
         }
         public IActionResult Index()
         {
-            List<KitapTuru> objKitapTuruList = _uygulamaDBContext.KitapTuruleri.ToList();       
+            List<KitapTuru> objKitapTuruList = _uygulamaDBContext.KitapTuruleri.ToList();
             return View(objKitapTuruList);
         }
 
@@ -28,20 +28,89 @@ namespace EgitimProjeAsp.Controllers
         public IActionResult Ekle(KitapTuru kitapTuru)
         {
 
-            _uygulamaDBContext.KitapTuruleri.Add(kitapTuru);
-            _uygulamaDBContext.SaveChanges(); //SaveChange yapılmazsa bilgiler veritabanına eklemez.
+            //_uygulamaDBContext.KitapTuruleri.Add(kitapTuru);
+            //_uygulamaDBContext.SaveChanges(); //SaveChange yapılmazsa bilgiler veritabanına eklemez.
 
-            return RedirectToAction("Index", "KitapTuru");
+            //return RedirectToAction("Index", "KitapTuru");
 
-            //if(ModelState.IsValid)
-            //{ 
+            if (ModelState.IsValid)
+            {
+
+                _uygulamaDBContext.KitapTuruleri.Add(kitapTuru);
+                _uygulamaDBContext.SaveChanges(); //SaveChange yapılmazsa bilgiler veritabanına eklemez.
+                TempData["basarili"] = "Yeni Kitap Türü başarılıyla oluşturuldu.";
+                return RedirectToAction("Index", "KitapTuru");
+            }
+            return View();
+        }
+
+        public IActionResult Guncelle(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            KitapTuru? kitapTuruVt = _uygulamaDBContext.KitapTuruleri.Find(id);
+
+            if (kitapTuruVt == null)
+            {
+                return NotFound();
+            }
+            return View(kitapTuruVt);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(KitapTuru kitapTuru)
+        {
 
             //_uygulamaDBContext.KitapTuruleri.Add(kitapTuru);
             //_uygulamaDBContext.SaveChanges(); //SaveChange yapılmazsa bilgiler veritabanına eklemez.
 
-            //return RedirectToAction("Index","KitapTuru");
-            //}
-            //return View();
+            //return RedirectToAction("Index", "KitapTuru");
+
+            if (ModelState.IsValid)
+            {
+
+                _uygulamaDBContext.KitapTuruleri.Update(kitapTuru);
+                _uygulamaDBContext.SaveChanges(); //SaveChange yapılmazsa bilgiler veritabanına eklemez.
+                TempData["basarili"] = "Yeni Kitap Türü başarılıyla güncellendi.";
+                return RedirectToAction("Index", "KitapTuru");
+            }
+            return View();
+        }
+
+        //GET ACTION
+        public IActionResult Sil(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            KitapTuru? kitapTuruVt = _uygulamaDBContext.KitapTuruleri.Find(id);
+
+            if (kitapTuruVt == null)
+            {
+                return NotFound();
+            }
+            return View(kitapTuruVt);
+        }
+
+        [HttpPost,ActionName("Sil")]
+        public IActionResult SilPost(int? id)
+        {
+            KitapTuru? kitapTuru = _uygulamaDBContext.KitapTuruleri.Find(id);
+            if(kitapTuru==null)
+            {
+                return NotFound(); 
+            }
+            _uygulamaDBContext.KitapTuruleri.Remove(kitapTuru);
+            _uygulamaDBContext.SaveChanges();
+            TempData["basarili"] = "Kayıt silme işlemi başarılı.";
+            return RedirectToAction("Index", "KitapTuru");
+
+
         }
 
     }
